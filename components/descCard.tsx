adc182204c52016092d2aa0593a1752440d8cf48
y-chan <y-chan@y-chan.dev>
2022-08-30 16:04:24 +0900

    using forward ref

--- components/descCard.tsx
+++ components/descCard.tsx
@@ -9,29 +9,26 @@ type Props = {
   children?: React.ReactNode
 }
 
-const descCard: React.FC<Props> = ({
-  title,
-  titleEn,
-  image,
-  imageAlt,
-  children,
-}) => {
-  return (
-    <div className="mx-8 my-4 desc-card-w">
-      <p className="text-left text-primary font-semibold my-2 2xl:text-xl">
-        {titleEn.toUpperCase()}
-      </p>
-      <p className="text-left font-semibold text-2xl 2xl:text-3xl my-2">
-        {title}
-      </p>
-      {children && (
-        <p className="text-left 2xl:text-xl md:h-20 2xl:h-16">{children}</p>
-      )}
-      <div>
-        <img className="rounded-xl" src={image.src} alt={imageAlt} />
+// eslint-disable-next-line react/display-name
+const descCard = React.forwardRef<HTMLDivElement, Props>(
+  ({ title, titleEn, image, imageAlt, children }, ref) => {
+    return (
+      <div className="mx-8 my-4 desc-card-w" ref={ref}>
+        <p className="text-left text-primary font-semibold my-2 2xl:text-xl">
+          {titleEn.toUpperCase()}
+        </p>
+        <p className="text-left font-semibold text-2xl 2xl:text-3xl my-2">
+          {title}
+        </p>
+        {children && (
+          <p className="text-left 2xl:text-xl md:h-20 2xl:h-16">{children}</p>
+        )}
+        <div>
+          <img className="rounded-xl" src={image.src} alt={imageAlt} />
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
 
 export default descCard
