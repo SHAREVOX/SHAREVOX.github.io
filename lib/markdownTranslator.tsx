b36805f255f9c2e8272eb45a31ad6420e62aff9d
task4233 <29667656+task4233@users.noreply.github.com>
2022-08-30 15:34:43 +0900

    [wip] add rehype-add-classes

--- lib/markdownTranslator.tsx
+++ lib/markdownTranslator.tsx
@@ -1,3 +1,4 @@
+import addClasses from 'rehype-add-classes'
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
 import remark2rehype from 'remark-rehype'
@@ -7,6 +8,9 @@ const markdownToHtml = async (markdownText: string): Promise<string> => {
   const result = await unified()
     .use(remarkParse) // markdown -> mdast の変換
     .use(remark2rehype) // mdast -> hast の変換
+    .use(addClasses, {
+      a: 'text-blue-600 hover:text-blue-800 visited:text-purple-600',
+    })
     .use(rehypeStringify) // hast -> html の変換
     .process(markdownText)
   return result.toString()
