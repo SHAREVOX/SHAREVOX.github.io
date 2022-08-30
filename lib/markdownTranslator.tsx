602b020b42823104a232d1df3075a9e91e3c6fd0
task4233 <29667656+task4233@users.noreply.github.com>
2022-08-30 15:23:34 +0900

    replace remark-html to remark-rehype

--- lib/markdownTranslator.tsx
+++ lib/markdownTranslator.tsx
@@ -1,8 +1,14 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse'
+import remark2rehype from 'remark-rehype'
+import { unified } from 'unified'
 
 const markdownToHtml = async (markdownText: string): Promise<string> => {
-  const result = await remark().use(html).process(markdownText)
+  const result = await unified()
+    .use(remarkParse) // markdown -> mdast の変換
+    .use(remark2rehype) // mdast -> hast の変換
+    .use(rehypeStringify) // hast -> html の変換
+    .process(markdownText)
   return result.toString()
 }
 
