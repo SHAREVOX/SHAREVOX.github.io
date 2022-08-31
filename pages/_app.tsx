f9ca5d562bcc342dd67b0b7d00d61457c5a1cd3c
y-chan <y-chan@y-chan.dev>
2022-09-01 01:15:07 +0900

    fix font load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,7 @@ import type { AppPropsWithLayout } from 'next/app'
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
   return getLayout(
-    <>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap"
-        rel="stylesheet"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap"
-        rel="stylesheet"
-      />
-      <Component {...pageProps} />
-    </>
+    <Component {...pageProps} />
   )
 }
 
