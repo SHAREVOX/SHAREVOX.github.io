8eeecadf3ff410d3a97c5aced4aae5c57a73d3dd
y-chan <y-chan@y-chan.dev>
2022-08-31 14:38:17 +0900

    set favion

--- components/header.tsx
+++ components/header.tsx
@@ -36,6 +36,32 @@ const Header: React.FC<Props> = ({ mainPageHeader, title }) => {
       <title>
         {title ? `SHAREVOX | ${title.length > 0 && title}` : `SHAREVOX`}
       </title>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicons/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+      <meta name="theme-color" content="#ffffff" />
       <meta
         name="description"
         content="無料で使える、声を作れるテキスト読み上げソフトウェア、SHAREVOX"
