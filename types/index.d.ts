c728053c5ec55d623f606b85883532e42335724b
y-chan <y-chan@y-chan.dev>
2022-09-10 12:00:06 +0900

    add type to global

--- types/index.d.ts
+++ types/index.d.ts
@@ -13,3 +13,20 @@ declare module 'next/app' {
     Component: NextPageWithLayout<P>
   }
 }
+
+declare global {
+  interface Window {
+    // pageviewのため
+    gtag(type: 'config', googleAnalyticsId: string, { page_path: string })
+    // eventのため
+    gtag(
+      type: 'event',
+      eventAction: string,
+      fieldObject: {
+        event_label: string
+        event_category: string
+        value?: string
+      }
+    )
+  }
+}
