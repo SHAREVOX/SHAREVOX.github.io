1cc9398d85fa671661aad66491203e7cca5fcfee
y-chan <y-chan@y-chan.dev>
2022-09-10 20:12:46 +0900

    set config

--- config.ts
+++ config.ts
@@ -5,13 +5,19 @@ const config = {
   GITHUB_CORE_URL: 'https://github.com/SHAREVOX/sharevox_core',
   RELEASE_NOTE_URL:
     'https://raw.githubusercontent.com/SHAREVOX/sharevox/main/public/updateInfos.json',
-  RELEASED_VERSION: '0.1.0',
-  WINDOWS_CPU_DOWNLOAD_URL: '#',
-  WINDOWS_CUDA_DOWNLOAD_URL: '#',
-  WINDOWS_DIRECTML_DOWNLOAD_URL: '#',
-  MAC_CPU_DOWNLOAD_URL: '#',
-  LINUX_CPU_DOWNLOAD_URL: '#',
-  LINUX_CUDA_DOWNLOAD_URL: '#',
+  RELEASED_VERSION: '0.1.1',
+  WINDOWS_CPU_DOWNLOAD_URL:
+    'https://github.com/SHAREVOX/sharevox/releases/download/0.1.1/SHAREVOX-CPU.Web.Setup.0.1.1.exe',
+  WINDOWS_CUDA_DOWNLOAD_URL:
+    'https://github.com/SHAREVOX/sharevox/releases/download/0.1.1/SHAREVOX-CUDA.Web.Setup.0.1.1.exe',
+  WINDOWS_DIRECTML_DOWNLOAD_URL:
+    'https://github.com/SHAREVOX/sharevox/releases/download/0.1.1/SHAREVOX.Web.Setup.0.1.1.exe',
+  MAC_CPU_DOWNLOAD_URL:
+    'https://github.com/SHAREVOX/sharevox/releases/download/0.1.1/SHAREVOX.0.1.1.dmg',
+  LINUX_CPU_DOWNLOAD_URL:
+    'https://github.com/SHAREVOX/sharevox/releases/download/0.1.1/sharevox-linux-cpu-0.1.1.tar.gz',
+  LINUX_CUDA_DOWNLOAD_URL:
+    'https://github.com/SHAREVOX/sharevox/releases/download/0.1.1/sharevox-linux-nvidia-0.1.1.tar.gz',
   OFFICIAL_TWITTER_URL: 'https://twitter.com/sharevox_pj',
   DEVELOPER_TWITTER_URL: 'https://twitter.com/y_chan_dev',
   FANBOX_URL: 'https://y-chan.fanbox.cc',
