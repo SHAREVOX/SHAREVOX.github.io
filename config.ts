ea6e1475c565ef8a85dad3044348192e9cf6e384
Yuto Ashida <y-chan@y-chan.dev>
2022-12-05 18:50:35 +0900

    update config

--- config.ts
+++ config.ts
@@ -1,23 +1,18 @@
+const RELEASED_VERSION = '0.1.6'
+
 const config = {
   GITHUB_ORG_URL: 'https://github.com/SHAREVOX',
   GITHUB_EDITOR_URL: 'https://github.com/SHAREVOX/sharevox',
   GITHUB_ENGINE_URL: 'https://github.com/SHAREVOX/sharevox_engine',
   GITHUB_CORE_URL: 'https://github.com/SHAREVOX/sharevox_core',
-  RELEASE_NOTE_URL:
-    'https://raw.githubusercontent.com/SHAREVOX/sharevox/sv-release-0.1/public/updateInfos.json',
-  RELEASED_VERSION: '0.1.5',
-  WINDOWS_CPU_DOWNLOAD_URL:
-    'https://github.com/SHAREVOX/sharevox/releases/download/0.1.5/SHAREVOX-CPU.Web.Setup.0.1.5.exe',
-  WINDOWS_CUDA_DOWNLOAD_URL:
-    'https://github.com/SHAREVOX/sharevox/releases/download/0.1.5/SHAREVOX-CUDA.Web.Setup.0.1.5.exe',
-  WINDOWS_DIRECTML_DOWNLOAD_URL:
-    'https://github.com/SHAREVOX/sharevox/releases/download/0.1.5/SHAREVOX.Web.Setup.0.1.5.exe',
-  MAC_CPU_DOWNLOAD_URL:
-    'https://github.com/SHAREVOX/sharevox/releases/download/0.1.5/SHAREVOX.0.1.5.dmg',
-  LINUX_CPU_DOWNLOAD_URL:
-    'https://github.com/SHAREVOX/sharevox/releases/download/0.1.5/sharevox-linux-cpu-0.1.5.tar.gz',
-  LINUX_CUDA_DOWNLOAD_URL:
-    'https://github.com/SHAREVOX/sharevox/releases/download/0.1.5/sharevox-linux-nvidia-0.1.5.tar.gz',
+  RELEASED_VERSION,
+  RELEASE_NOTE_URL: `https://raw.githubusercontent.com/SHAREVOX/sharevox/${RELEASED_VERSION}/public/updateInfos.json`,
+  WINDOWS_CPU_DOWNLOAD_URL: `https://github.com/SHAREVOX/sharevox/releases/download/${RELEASED_VERSION}/SHAREVOX-CPU.Web.Setup.${RELEASED_VERSION}.exe`,
+  WINDOWS_CUDA_DOWNLOAD_URL: `https://github.com/SHAREVOX/sharevox/releases/download/${RELEASED_VERSION}/SHAREVOX-CUDA.Web.Setup.${RELEASED_VERSION}.exe`,
+  WINDOWS_DIRECTML_DOWNLOAD_URL: `https://github.com/SHAREVOX/sharevox/releases/download/${RELEASED_VERSION}/SHAREVOX.Web.Setup.${RELEASED_VERSION}.exe`,
+  MAC_CPU_DOWNLOAD_URL: `https://github.com/SHAREVOX/sharevox/releases/download/${RELEASED_VERSION}/SHAREVOX.${RELEASED_VERSION}.dmg`,
+  // LINUX_CPU_DOWNLOAD_URL: `https://github.com/SHAREVOX/sharevox/releases/download/${RELEASED_VERSION}/sharevox-linux-cpu-${RELEASED_VERSION}.tar.gz`,
+  // LINUX_CUDA_DOWNLOAD_URL: `https://github.com/SHAREVOX/sharevox/releases/download/${RELEASED_VERSION}/sharevox-linux-nvidia-${RELEASED_VERSION}.tar.gz`,
   OFFICIAL_TWITTER_URL: 'https://twitter.com/sharevox_pj',
   DEVELOPER_TWITTER_URL: 'https://twitter.com/y_chan_dev',
   FANBOX_URL: 'https://y-chan.fanbox.cc',
